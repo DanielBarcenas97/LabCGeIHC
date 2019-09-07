f1fa319fcd7f5b19c17a592ee0d4e5dfaa0bd3d9
DanielBarcenas97 <barcenas.proteco@gmail.com>
2019-09-07 13:58:16 -0500

    Letras Negras

--- 03-SistemasCoordenados/src/main.cpp
+++ 03-SistemasCoordenados/src/main.cpp
@@ -117,10 +117,10 @@ void init(int width, int height, std::string strTitle, bool bFullScreen) {
 
 	Vertex vertices2[] =
 	{
-		{ glm::vec3(-0.5f, -0.5f,  0.5f), glm::vec3(1.0f, 0.0f, 0.0f) },
-		{ glm::vec3(0.5f, -0.5f,  0.5f), glm::vec3(1.0f, 0.0f, 0.0f) },
-		{ glm::vec3(0.5f, 0.5f,  0.5f), glm::vec3(1.0f, 0.0f, 0.0f) },
-		{ glm::vec3(-0.5f, 0.5f, 0.5f), glm::vec3(1.0f, 0.0f, 0.0f) },
+		{ glm::vec3(-0.5f, -0.5f,  0.5f), glm::vec3(0.0f, 0.0f, 0.0f) },
+		{ glm::vec3(0.5f, -0.5f,  0.5f), glm::vec3(0.0f, 0.0f, 0.0f) },
+		{ glm::vec3(0.5f, 0.5f,  0.5f), glm::vec3(0.0f, 0.0f, 0.0f) },
+		{ glm::vec3(-0.5f, 0.5f, 0.5f), glm::vec3(0.0f, 0.0f, 0.0f) },
 		{ glm::vec3(0.5f, -0.5f, 0.5f), glm::vec3(0.0f, 1.0f, 0.0f) },
 		{ glm::vec3(0.5f, -0.5f, -0.5f), glm::vec3(0.0f, 1.0f, 0.0f) },
 		{ glm::vec3(0.5f, 0.5f, -0.5f), glm::vec3(0.0f, 1.0f, 0.0f) },
